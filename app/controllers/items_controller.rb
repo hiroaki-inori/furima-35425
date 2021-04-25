d697445630d036c1402a3b0f0c84d08b5ac6bae7
hiroaki-inori <zanetti585@gmail.com>
2021-04-25 22:12:42 +0900

    ActiveHashの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,6 +9,7 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
+    @item.save
   end
 
   private
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,22 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :delivery_charge
+  belongs_to :prefecture
+  belongs_to :shipping_day
+  belongs_to :user
+  has_one :buy
+
+  has_one_attached :image
 
   validates :item_name,           presence: true
   validates :item_text,           presence: true
-  validates :category_id,         presence: true
-  validates :status_id,           presence: true
-  validates :delivery_charge_id,  presence: true
-  validates :prefecture_id,       presence: true
-  validates :shipping_day_id,     presence: true
+  validates :category_id,         numericality: { other_than: 1 }
+  validates :status_id,           numericality: { other_than: 1 }
+  validates :delivery_charge_id,  numericality: { other_than: 1 }
+  validates :prefecture_id,       numericality: { other_than: 1 }
+  validates :shipping_day_id,     numericality: { other_than: 1 }
   validates :price,               presence: true
-  
-  has_one_attached :image
 
-  belongs_to :user
-  has_one :buy
 end
