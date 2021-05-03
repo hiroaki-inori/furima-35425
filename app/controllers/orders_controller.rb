45e9d01c6ccf4ae0e39f2e32285e6830b743d40f
hiroaki-inori <zanetti585@gmail.com>
2021-05-03 12:19:33 +0900

    修正2

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
-  before_action :login_myitem_solditem, only: [:index]
   before_action :set_item, only: [:index, :create]
+  before_action :login_myitem_solditem, only: [:index, :create]
 
   def index
     @order_delivery = OrderDelivery.new
@@ -19,11 +19,14 @@ class OrdersController < ApplicationController
   end
 
   private
+  
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
 
   def order_params
-    item = Item.find(params[:item_id])
     params.require(:order_delivery).permit(:postcode, :prefecture_id, :city, :block, :building, :tel).merge(
-      user_id: current_user.id, item_id: params[:item_id], token: params[:token], price: item.price
+      user_id: current_user.id, item_id: params[:item_id], token: params[:token], price: @item.price
     )
   end
 
@@ -37,11 +40,7 @@ class OrdersController < ApplicationController
   end
 
   def login_myitem_solditem
-    item = Item.find(params[:item_id])
-    redirect_to root_path if current_user.id == item.user.id || item.order
+    redirect_to root_path if current_user.id == @item.user.id || @item.order
   end
 
-  def set_item
-    @item = Item.find(params[:item_id])
-  end
 end
